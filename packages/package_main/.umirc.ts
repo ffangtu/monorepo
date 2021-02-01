f18d0f53156286bfe7175dc1feb90e770cad9632
zhangkun <762845835@qq.com>
2021-02-01 22:18:48 +0800

    save

---
+++ packages/package_main/.umirc.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'umi';
+import path from 'path'
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+  ],
+  fastRefresh: {}
+});
